a453d389e1b52e20748ca83ddd8b0faebb04f5aa
Duyet Le <5009534+duyet@users.noreply.github.com>
2022-07-26 00:19:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var path = require('path');
 var words = fs.readFileSync(path.join(__dirname, './vietnamese-stopwords.txt'), 'utf8');
 
 module.exports = words.split('\n').map(function(word) {
-        return (word || '').trim();
+  return (word || '').trim();
 }).filter(function(word) {
-        return word !== false && word.length > 0;
+  return word !== false && word.length > 0;
 })
